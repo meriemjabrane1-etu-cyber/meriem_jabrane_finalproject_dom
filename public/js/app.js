fa5c786a92f4c4dfda9336f8d765ccf710005704
meriemjabrane1-etu-cyber <meriem.jabrane1-etu@etu.univh2c.ma>
2026-01-20 14:23:29 +0100

    add_style_hero

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,32 @@ let navLinks = document.getElementById("navLinks")
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show")
 })
+
+let dropdownBtn = document.getElementById("dropdownBtn")
+let dropdownMenu = document.getElementById("dropdownMenu")
+
+let deepBtn = document.getElementById("deepBtn")
+let deepMenu = document.getElementById("deepMenu")
+
+dropdownBtn.addEventListener("click", function (e) {
+  e.preventDefault()
+
+  dropdownMenu.classList.toggle("show")
+
+  if (!dropdownMenu.classList.contains("show")) {
+    deepMenu.classList.remove("show")
+  }
+})
+
+deepBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  deepMenu.classList.toggle("show");
+});
+
+document.addEventListener("click", function (e) {
+  if (!e.target.closest(".dropdown")) {
+    dropdownMenu.classList.remove("show");
+    deepMenu.classList.remove("show");
+  }
+})
