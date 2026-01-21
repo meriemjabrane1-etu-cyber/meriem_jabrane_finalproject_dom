ee48989a122f4eafe568269a951ee6e02c73cecf
meriemjabrane1-etu-cyber <meriem.jabrane1-etu@etu.univh2c.ma>
2026-01-21 08:08:27 +0100

    add_section_About

--- public/js/app.js
+++ public/js/app.js
@@ -33,3 +33,56 @@ document.addEventListener("click", function (e) {
     deepMenu.classList.remove("show");
   }
 })
+// ~~ BOOKING MODAL
+let bookBtn = document.querySelector(".btnNav"); // Book a Table button
+let bookingModal = document.getElementById("bookingModal");
+let closeBooking = document.getElementById("closeBooking");
+let bookingForm = document.getElementById("bookingForm");
+let bookingMessage = document.getElementById("bookingMessage");
+let bookings = [];
+
+bookBtn.addEventListener("click", () => {
+  bookingModal.style.display = "flex";
+});
+
+closeBooking.addEventListener("click", () => {
+  bookingModal.style.display = "none";
+});
+
+bookingForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let name = document.getElementById("fullName").value;
+  let meal = document.getElementById("meal").value;
+  let start = document.getElementById("startTime").value;
+  let end = document.getElementById("endTime").value;
+  let people = document.getElementById("people").value;
+
+
+  function timeToMinutes(t) {
+    let [h, m] = t.split(":");
+    return parseInt(h) * 60 + parseInt(m);
+  }
+
+  let startMin = timeToMinutes(start);
+  let endMin = timeToMinutes(end);
+
+
+  let conflict = bookings.some(b => {
+    if (b.meal !== meal) return false;
+    let bStart = timeToMinutes(b.start);
+    let bEnd = timeToMinutes(b.end);
+    return (startMin < bEnd && endMin > bStart);
+  });
+
+  if (conflict) {
+    bookingMessage.textContent = "Time slot already booked!";
+    bookingMessage.style.color = "red";
+  } else {
+    bookings.push({name, meal, start, end, people});
+    bookingMessage.textContent = "Booking confirmed!";
+    bookingMessage.style.color = "green";
+    bookingForm.reset();
+  }
+});
+
