a2b4baada800fa66037cb0fde6f8a6e0762657f0
meriemjabrane1-etu-cyber <meriem.jabrane1-etu@etu.univh2c.ma>
2026-01-21 15:57:17 +0100

    des_modification_modal

--- public/js/app.js
+++ public/js/app.js
@@ -22,32 +22,69 @@ dropdownBtn.addEventListener("click", function (e) {
 })
 
 deepBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  deepMenu.classList.toggle("show");
+  e.preventDefault()
+  deepMenu.classList.toggle("show")
 });
 
 document.addEventListener("click", function (e) {
   if (!e.target.closest(".dropdown")) {
-    dropdownMenu.classList.remove("show");
-    deepMenu.classList.remove("show");
+    dropdownMenu.classList.remove("show")
+    deepMenu.classList.remove("show")
   }
 })
 // ~~ BOOKING MODAL
-let openBtn = document.getElementById("openModal")
+let bookings = []
 let modal = document.getElementById("modal")
-let closeBtn = document.querySelector(".close")
+let openBtn = document.getElementById("openModal")
+let closeBtn = document.querySelector(".close") 
+let form = document.querySelector(".modal-form")
 
-openBtn.onclick = () => {
+let dateInput = form.querySelector('input[type="date"]')
+let timeInput = form.querySelector('input[type="time"]')
+let peopleInput = form.querySelector('input[type="number"]')
+openBtn.addEventListener("click", () => {
   modal.style.display = "flex"
-}
+})
 
-closeBtn.onclick = () => {
+closeBtn.addEventListener("click", () => {
   modal.style.display = "none"
-}
-
-window.onclick = (e) => {
-  if (e.target === modal) {
-    modal.style.display = "none"
+})
+function isOverlapping(newBooking) {
+  for (let booking of bookings) {
+    if (booking.date === newBooking.date && newBooking.time === booking.time) {
+      return true
+    }
   }
+  return false
 }
+form.addEventListener("submit", function (e) {
+  e.preventDefault()
+
+  let date = dateInput.value
+  let time = timeInput.value
+  let people = peopleInput.value
+
+  if (!date || !time || !people) {
+    alert(" Fill all required fields")
+    return
+  }
+
+  let newBooking = {
+    date: date,
+    time: time
+  }
+
+  if (isOverlapping(newBooking)) {
+    alert(" This time is already booked")
+    return
+  }
+
+  bookings.push(newBooking)
+
+  alert(" Booking successful")
+
+  form.reset()
+  modal.style.display = "none"
+})
+
+
